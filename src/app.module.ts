e74523fece390ad54227f2678d0be293095c237a
hojuncha <hojun.cha997@gmail.com>
2024-10-25 22:15:17 +0900

    feat: 앱모듈에서 직접 컨트롤러, 프로바이더 등록 제거 (CatsModule로 대체)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
     // CatsModule을 구성하고 가져옴으로 인해서 CatsController와 CatsService를 직접 등록해줄 필요가 없다.
   imports: [CatsModule], 
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
