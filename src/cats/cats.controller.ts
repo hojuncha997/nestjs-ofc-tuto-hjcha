4d6badee926861861e4e467bae9806c43e5bce28
hojuncha <hojun.cha997@gmail.com>
2024-11-03 23:46:22 +0900

    feat: 표준예외(HttpException) 실습

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
@@ -24,8 +24,36 @@ export class CatsController {
 
   */
 
+  // @Get()
+  // async findAll(): Promise<Cat[]> {
+  //   return this.catsService.findAll();
+  // }
+
+  // @Get()
+  // async findAll() {
+  //   throw new HttpException('Forbidden', HttpStatus.FORBIDDEN)
+    // HttpException은 예외처리를 위해 NestJs에서 제공하는 표준 에러 처리 클래스이다.
+    // 첫 번째 파라미터는 response(-> message))로 response body에 들어갈 스트링 또는 오브젝트 형태의 값을 넘겨야 한다. 오브젝트의 경우 nest.js에서 자동 직렬화.
+    // 두 번째 파라미터는 상태 코드(statusCode)이다. HttpStatus의 enum을 사용하는 것이 권장된다. 이 두 파라미터는 필수로 입력해 줘야 한다.
+    // 세 번째 파라미터는 options이다. 에러 원인을 제공하는 데 주로 사용되며 자동으로 직렬화 되지 않는다.
+    // {"statusCode":403,"message":"Forbidden"}
+  // }
+
+
   @Get()
-  async findAll(): Promise<Cat[]> {
-    return this.catsService.findAll();
+  async findAll() : Promise<Cat[]> {
+    try {
+      throw new Error() // 일부러 에러 발생시키기.
+      return await this.catsService.findAll()
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'This is a custom message',
+      }, HttpStatus.FORBIDDEN, {
+        cause: error
+      });
+    }
   }
+
+
 }
